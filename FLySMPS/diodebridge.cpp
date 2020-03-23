5ee0cca640298b05557179354f0c8dfa49db73d0
aemeltsev <h002mes@gmail.com>
2020-03-23 23:04:34 +0300

    add prototype
    add prototype for DiodeBridge class

--- FLySMPS/diodebridge.cpp
+++ FLySMPS/diodebridge.cpp
@@ -1,6 +1,97 @@
 #include "diodebridge.h"
+#define PI 3.1415926535897932385;
 
 DiodeBridge::DiodeBridge()
 {
 
+}
+
+/**
+  * @brief  None
+  * @param  None
+  * @retval None
+  */
+double DiodeBridge::IDiodePeak()
+{
+    return PI+PI;
+}
+
+/**
+  * @brief  None
+  * @param  None
+  * @retval None
+  */
+double DiodeBridge::IDiodeRMS()
+{
+    return PI+PI;
+}
+
+/**
+  * @brief  None
+  * @param  None
+  * @retval None
+  */
+double DiodeBridge::IDiodeAVG()
+{
+    return PI+PI;
+}
+
+/**
+  * @brief  None
+  * @param  None
+  * @retval None
+  */
+double DiodeBridge::IDiodeRMSTot()
+{
+    return PI+PI;
+}
+
+/**
+  * @brief  None
+  * @param  None
+  * @retval None
+  */
+double DiodeBridge::ILoadAVG()
+{
+    return PI+PI;
+}
+
+/**
+  * @brief  None
+  * @param  None
+  * @retval None
+  */
+double DiodeBridge::DiodeConductTime()
+{
+    return PI+PI;
+}
+
+/**
+  * @brief  None
+  * @param  None
+  * @retval None
+  */
+double DiodeBridge::DiodeCurrentSlope()
+{
+    return PI+PI;
+}
+
+/**
+  * @brief  None
+  * @param  None
+  * @retval None
+  */
+double DiodeBridge::MinPeakInVoltage()
+{
+    return PI+PI;
+}
+
+/**
+  * @brief  None
+  * @param  None
+  * @retval None
+  */
+double DiodeBridge::MaxPeakInVoltage()
+{
+    return PI+PI;
 }
--- FLySMPS/diodebridge.h
+++ FLySMPS/diodebridge.h
@@ -1,11 +1,26 @@
 #ifndef DIODEBRIDGE_H
 #define DIODEBRIDGE_H
-
+#include <stdint.h>
+#include <math.h>
 
 class DiodeBridge
 {
 public:
     DiodeBridge();
+
+    double IDiodePeak();
+    double IDiodeRMS();
+    double IDiodeAVG();
+
+    double IDiodeRMSTot();
+
+    double ILoadAVG();
+
+    double DiodeConductTime();
+    double DiodeCurrentSlope();
+
+    double MinPeakInVoltage();
+    double MaxPeakInVoltage();
 };
 
 #endif // DIODEBRIDGE_H
