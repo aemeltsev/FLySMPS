8bbc22ccdb26e65286b8f1ef833958fda47f0ffd
Tony Emeltsev <h002mes@gmail.com>
2020-06-02 17:46:44 +0300

    replace ptr to ref

--- FLySMPS/bulkcap.cpp
+++ FLySMPS/bulkcap.cpp
@@ -10,16 +10,15 @@ BulkCap::BulkCap()
 
 }
 
-double BulkCap::VRRMS(InputValue* ivalue)
+double BulkCap::VRRMS(InputValue &ivalue)
 {
-    return ((ivalue->input_volt_ac_min)*(1./(sqrt(2))))-((ivalue->input_volt_ac_min)*(2./S_PI));
+    return ((ivalue.input_volt_ac_min)*(1./(sqrt(2))))-((ivalue.input_volt_ac_min)*(2./S_PI));
 }
 
-double BulkCap::VDOut(InputValue* ivalue)
+double BulkCap::VDOut(InputValue &ivalue)
 {
-    return ((ivalue->input_volt_ac_min)*sqrt(2.))-VRRMS(ivalue);
+    return ((ivalue.input_volt_ac_min)*sqrt(2.))-VRRMS(ivalue);
 }
-
 /**
   * @brief The total refueling time from Vmin to Vpeak
   * @param VInMin - minimum voltage value after diode blidge and capacitor
@@ -27,11 +26,10 @@ double BulkCap::VDOut(InputValue* ivalue)
   * @param FLine - frequency in power line
   * @retval DeltaT - Calculating time value from Vmin to Vpeak
   */
-double BulkCap::DeltaT(InputValue* ivalue)
+double BulkCap::DeltaT(InputValue &ivalue)
 {
-    return (asin(VDOut(ivalue)/(ivalue->input_volt_ac_min*sqrt(2))))/(2.*S_PI*(ivalue->freq_line));
+    return (asin(VDOut(ivalue)/(ivalue.input_volt_ac_min*sqrt(2))))/(2.*S_PI*(ivalue.freq_line));
 }
-
 /**
   * @brief The total charging time
   * @param VInMin - minimum voltage value after diode blidge and capacitor
@@ -39,11 +37,10 @@ double BulkCap::DeltaT(InputValue* ivalue)
   * @param FLine - frequency in power line
   * @retval ChargTime - total charging time value
   */
-double BulkCap::ChargTime(InputValue* ivalue)
+double BulkCap::ChargTime(InputValue &ivalue)
 { 
-    return ((1./(4.*(ivalue->freq_line)))-(DeltaT(ivalue)));
+    return ((1./(4.*(ivalue.freq_line)))-(DeltaT(ivalue)));
 }
-
 /**
   * @brief Calculate the bulk capacitor value
   * @param VInMin - minimum voltage value after diode blidge and capacitor
@@ -53,11 +50,10 @@ double BulkCap::ChargTime(InputValue* ivalue)
   * @param Eff - efficiency
   * @retval CapValue - bulk capacitor value
   */
-double BulkCap::CapValue(InputValue* ivalue)
+double BulkCap::CapValue(InputValue &ivalue)
 {
-    return ((2.*(ivalue->power_out_max))*(1./(4.*ivalue->freq_line))+(DeltaT(ivalue)))/((ivalue->eff)*((VRRMS(ivalue)*VRRMS(ivalue))-(VDOut(ivalue)*VDOut(ivalue))));
+    return ((2.*(ivalue.power_out_max))*(1./(4.*ivalue.freq_line))+(DeltaT(ivalue)))/((ivalue.eff)*((VRRMS(ivalue)*VRRMS(ivalue))-(VDOut(ivalue)*VDOut(ivalue))));
 }
-
 /**
   * @brief Load peak current value
   * @param POut - summary output power of the converter
@@ -65,11 +61,10 @@ double BulkCap::CapValue(InputValue* ivalue)
   * @param VInMinRMS - Minimum RMS value line voltage
   * @retval ILoadMax - peak current value
   */
-double BulkCap::ILoadMax(InputValue* ivalue, DBridge* dbvalue)
+double BulkCap::ILoadMax(InputValue &ivalue, DBridge &dbvalue)
 {
-    return (ivalue->power_out_max)/((ivalue->eff)*(dbvalue->in_min_rms_voltage));
+    return (ivalue.power_out_max)/((ivalue.eff)*(dbvalue.in_min_rms_voltage));
 }
-
 /**
   * @brief Load minimum current value
   * @param POut - summary output power of the converter
@@ -77,11 +72,10 @@ double BulkCap::ILoadMax(InputValue* ivalue, DBridge* dbvalue)
   * @param VInMaxRMS - Maximum RMS value line voltage
   * @retval ILoadMin - minimum current value
   */
-double BulkCap::ILoadMin(InputValue* ivalue, DBridge* dbvalue)
+double BulkCap::ILoadMin(InputValue &ivalue, DBridge &dbvalue)
 {
-    return (ivalue->power_out_max)/((ivalue->eff)*(dbvalue->in_max_rms_voltage));
+    return (ivalue.power_out_max)/((ivalue.eff)*(dbvalue.in_max_rms_voltage));
 }
-
 /**
   * @brief The bulk capacitor peak current
   * @param CapVal - bulk capacitor value
@@ -90,11 +84,10 @@ double BulkCap::ILoadMin(InputValue* ivalue, DBridge* dbvalue)
   * @param FLine - frequency in power line
   * @retval IBulkCapPeak - bulk capacitor peak current
   */
-double BulkCap::IBulkCapPeak(BCap* bcvalue, InputValue* ivalue)
+double BulkCap::IBulkCapPeak(BCap &bcvalue, InputValue &ivalue)
 {
-    return (2.*S_PI*(ivalue->freq_line)*(bcvalue->bcapacitor_value)*(VDOut(ivalue))*(cos(2.*S_PI*(ivalue->freq_line)*DeltaT(ivalue))));
+    return (2.*S_PI*(ivalue.freq_line)*(bcvalue.bcapacitor_value)*(VDOut(ivalue))*(cos(2.*S_PI*(ivalue.freq_line)*DeltaT(ivalue))));
 }
-
 /**
   * @brief Calculate bulk capacitor RMS current value
   * @param ILoadAVG - diode average current
@@ -102,11 +95,10 @@ double BulkCap::IBulkCapPeak(BCap* bcvalue, InputValue* ivalue)
   * @param FLine - frequency in power line
   * @retval IBulkCapRMS - bulk capacitor RMS current
   */
-double BulkCap::IBulkCapRMS(DBridge *dbvalue, InputValue* ivalue)
+double BulkCap::IBulkCapRMS(DBridge &dbvalue, InputValue &ivalue)
 {
-    return dbvalue->load_avg_curr*(sqrt((2./(3.*(ivalue->freq_line)*(dbvalue->diode_cond_time)))-1));
+    return dbvalue.load_avg_curr*(sqrt((2./(3.*(ivalue.freq_line)*(dbvalue.diode_cond_time)))-1));
 }
-
 /**
   * @brief Recalculation after input capacitor selection
   * @param VInMin - minimum voltage value after diode blidge and capacitor
@@ -116,18 +108,17 @@ double BulkCap::IBulkCapRMS(DBridge *dbvalue, InputValue* ivalue)
   * @param FLine - frequency in power line
   * @retval VMinInp - recalculation after input capacitor selection
   */
-double BulkCap::VMinInp(BCap* bcvalue, InputValue* ivalue)
+double BulkCap::VMinInp(BCap &bcvalue, InputValue &ivalue)
 {
-    return sqrt((VDOut(ivalue)*VDOut(ivalue))-((2.*(ivalue->power_out_max)*((1./(4.*ivalue->freq_line)-DeltaT(ivalue))))/(bcvalue->bcapacitor_value)));
+    return sqrt((VDOut(ivalue)*VDOut(ivalue))-((2.*(ivalue.power_out_max)*((1./(4.*ivalue.freq_line)-DeltaT(ivalue))))/(bcvalue.bcapacitor_value)));
 }
-
 /**
   * @brief VDCMin simply the average value of MinIng and VRectMinPeak
   * @param VRectMinPeak - peak voltage value after diode blidge and capacitor
   * @param VMinInp - recalculation after input capacitor selection
   * @retval VDCMin - simply the average value of MinIng and VRectMinPeak
   */
-double BulkCap::VDCMin(BCap* bcvalue, InputValue* ivalue)
+double BulkCap::VDCMin(BCap &bcvalue, InputValue &ivalue)
 {
-    return (VDOut(ivalue)+(bcvalue->input_min_voltage))/2.;
+    return (VDOut(ivalue)+(bcvalue.input_min_voltage))/2.;
 }
--- FLySMPS/bulkcap.h
+++ FLySMPS/bulkcap.h
@@ -14,22 +14,22 @@ class BulkCap
 public:
     BulkCap();
 
-    double DeltaT(InputValue *ivalue);
-    double ChargTime(InputValue *ivalue);
+    double DeltaT(InputValue &ivalue);
+    double ChargTime(InputValue &ivalue);
 
-    double CapValue(InputValue *ivalue);
+    double CapValue(InputValue &ivalue);
 
-    double ILoadMax(InputValue* ivalue, DBridge* dbvalue);
-    double ILoadMin(InputValue* ivalue, DBridge* dbvalue);
-    double IBulkCapPeak(BCap *bcvalue, InputValue *ivalue);
-    double IBulkCapRMS(DBridge *dbvalue, InputValue* ivalue);
+    double ILoadMax(InputValue &ivalue, DBridge &dbvalue);
+    double ILoadMin(InputValue &ivalue, DBridge &dbvalue);
+    double IBulkCapPeak(BCap &bcvalue, InputValue &ivalue);
+    double IBulkCapRMS(DBridge &dbvalue, InputValue &ivalue);
 
-    double VMinInp(BCap* bcvalue, InputValue* ivalue);
-    double VDCMin(BCap *bcvalue, InputValue *ivalue);
+    double VMinInp(BCap &bcvalue, InputValue &ivalue);
+    double VDCMin(BCap &bcvalue, InputValue &ivalue);
 
 private:
-    double VDOut(InputValue* ivalue);
-    double VRRMS(InputValue* ivalue);
+    double VDOut(InputValue &ivalue);
+    double VRRMS(InputValue &ivalue);
 
 };
 
