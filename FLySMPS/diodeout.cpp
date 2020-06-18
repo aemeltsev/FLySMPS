807528f9673dd5673ac9d232704d89ee251cbe9c
Tony Emeltsev <h002mes@gmail.com>
2020-06-18 20:44:20 +0300

    fields and methods

--- FLySMPS/diodeout.cpp
+++ FLySMPS/diodeout.cpp
@@ -1,6 +1,11 @@
 #include "diodeout.h"
 
-DiodeOut::DiodeOut()
+double DiodeOut::doDiodeRevVolt(const DBridge& dbvalue)
 {
+    return (volt_out+dbvalue.in_max_rms_voltage)/turn_ratio;
+}
 
+double DiodeOut::doDiodePowLoss(const InputValue& ivalue)
+{
+    return (power_sec*ivalue.volt_diode_drop_sec)/volt_out;
 }
--- FLySMPS/diodeout.h
+++ FLySMPS/diodeout.h
@@ -1,11 +1,19 @@
 #ifndef DIODEOUT_H
 #define DIODEOUT_H
-
+#include <cmath>
+#include <cstdint>
+#include <structdata.h>
 
 class DiodeOut
 {
 public:
-    DiodeOut();
+    DiodeOut(double ps, double vo, double tr): power_sec(ps), volt_out(vo), turn_ratio(tr){}
+    double doDiodeRevVolt(const DBridge& dbvalue);
+    double doDiodePowLoss(const InputValue& ivalue);
+private:
+    double power_sec;
+    double volt_out;
+    double turn_ratio;
 };
 
 #endif // DIODEOUT_H
