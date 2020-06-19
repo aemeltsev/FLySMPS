d72a1f35d46a483756bc231c500e2addaddb63c9
Tony Emeltsev <h002mes@gmail.com>
2020-06-19 15:05:34 +0300

    add fields, methods

--- FLySMPS/capout.cpp
+++ FLySMPS/capout.cpp
@@ -1,6 +1,32 @@
 #include "capout.h"
 
-CapOut::CapOut()
+double CapOut::ocESRCapOut()
 {
+    return (volts_rippl*esr_perc)/curr_peak_out;
+}
 
+double CapOut::ocTimeCapCharg(const InputValue &ivalue)
+{
+    double cross_frq = cros_frq_start_val*ivalue.freq_switch;
+    return (1/(4*cross_frq))+(1/ivalue.freq_switch);
+}
+
+double CapOut::ocCurrCap()
+{
+    return curr_peak_out/2;
+}
+
+double CapOut::ocCapOutValue(const InputValue& ivalue)
+{
+    return (ocCurrCap()*ocTimeCapCharg(ivalue))/(2*(volts_out*volts_rippl));
+}
+
+double CapOut::ocCurrOurRMS(const FBTransformer& fbtvalue)
+{
+    return curr_peak_out*sqrt((fbtvalue.actual_max_duty_cycle/3))-curr_peak_out;
+}
+
+double CapOut::ocCapOutLoss(const FBTransformer& fbtvalue)
+{
+    return pow(ocCurrOurRMS(fbtvalue), 2)*ocESRCapOut();
 }
--- FLySMPS/capout.h
+++ FLySMPS/capout.h
@@ -1,11 +1,26 @@
 #ifndef CAPOUT_H
 #define CAPOUT_H
-
+#include <cmath>
+#include <cstdint>
+#include <structdata.h>
 
 class CapOut
 {
 public:
-    CapOut();
+    CapOut(double vr, double esr, double cpo, double vo, double crfq): volts_rippl(vr), esr_perc(esr), curr_peak_out(cpo), volts_out(vo), cros_frq_start_val(crfq){}
+    double ocESRCapOut();
+    double ocCapOutValue(const InputValue& ivalue);
+    double ocCurrOurRMS(const FBTransformer& fbtvalue);
+    double ocCapOutLoss(const FBTransformer &fbtvalue);
+
+private:
+    double volts_rippl;
+    double esr_perc;
+    double curr_peak_out;
+    double volts_out;
+    double cros_frq_start_val;//good starting value for crossover frequency(1/20 to 1/10)
+    double ocTimeCapCharg(const InputValue& ivalue);
+    double ocCurrCap();
 };
 
 #endif // CAPOUT_H
