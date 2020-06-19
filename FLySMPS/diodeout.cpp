0c3c100ecbbb5ac179de38d9993cc068e4b36742
Tony Emeltsev <h002mes@gmail.com>
2020-06-19 09:40:11 +0300

    comments

--- FLySMPS/diodeout.cpp
+++ FLySMPS/diodeout.cpp
@@ -1,10 +1,18 @@
 #include "diodeout.h"
-
+/**
+  * @brief calc diode reverse voltage
+  * @param dbvalue input max rms voltage
+  * @return max reverse voltage
+  */
 double DiodeOut::doDiodeRevVolt(const DBridge& dbvalue)
 {
     return (volt_out+dbvalue.in_max_rms_voltage)/turn_ratio;
 }
-
+/**
+  * @brief losses on the diode
+  * @param ivalue volt drop on schottky diode
+  * @return losses value
+  */
 double DiodeOut::doDiodePowLoss(const InputValue& ivalue)
 {
     return (power_sec*ivalue.volt_diode_drop_sec)/volt_out;
