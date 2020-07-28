407f87d4475773940e3843ec58ad5dd0fdd9014f
Tony Emeltsev <aemeltsev@gmail.com>
2020-07-28 17:12:48 +0300

    Review CapOut + zero frequency cap output + ripple voltage

--- FLySMPS/capout.cpp
+++ FLySMPS/capout.cpp
@@ -2,10 +2,9 @@
 
 /**
   * @brief select output ESR based on the allowable output ripple voltage
-  * @param
   * @return return equivalent series resistance
   */
-double CapOut::ocESRCapOut()
+inline double CapOut::ocESRCapOut()
 {
     return (volts_rippl*esr_perc)/curr_peak_out;
 }
@@ -14,17 +13,16 @@ double CapOut::ocESRCapOut()
   * @param freq_switch - operatin switch frequency
   * @return charg time
   */
-double CapOut::ocTimeCapCharg(const InputValue &ivalue)
+inline double CapOut::ocTimeCapCharg(const InputValue &ivalue)
 {
     double cross_frq = cros_frq_start_val*ivalue.freq_switch;
     return (1/(4*cross_frq))+(1/ivalue.freq_switch);
 }
 /**
   * @brief peak-to-peak ripple current value
-  * @param
   * @return current p-t-p
   */
-double CapOut::ocCurrCap()
+inline double CapOut::ocCurrCap()
 {
     return curr_peak_out/2;
 }
@@ -33,7 +31,7 @@ double CapOut::ocCurrCap()
   * @param ivalue for time charging calculation
   * @return output capacitor
   */
-double CapOut::ocCapOutValue(const InputValue& ivalue)
+inline double CapOut::ocCapOutValue(const InputValue& ivalue)
 {
     return (ocCurrCap()*ocTimeCapCharg(ivalue))/(2*(volts_out*volts_rippl));
 }
@@ -42,7 +40,7 @@ double CapOut::ocCapOutValue(const InputValue& ivalue)
   * @param actual_max_duty_cycle actual switching duty cycle
   * @return rms current
   */
-double CapOut::ocCurrOurRMS(const FBPT &fbptval)
+inline double CapOut::ocCurrOurRMS(const FBPT &fbptval)
 {
     return curr_peak_out*sqrt((fbptval.actual_max_duty_cycle/3))-curr_peak_out;
 }
@@ -51,7 +49,25 @@ double CapOut::ocCurrOurRMS(const FBPT &fbptval)
   * @param actual_max_duty_cycle for rms current
   * @return losses
   */
-double CapOut::ocCapOutLoss(const FBPT &fbptval)
+inline double CapOut::ocCapOutLoss(const FBPT &fbptval)
 {
     return pow(ocCurrOurRMS(fbptval), 2)*ocESRCapOut();
+}
+/**
+ * @brief
+ * @param
+ * @return
+ */
+inline double CapOut::ocZeroFreqCapOut(const InputValue& ivalue)
+{
+    return 1./(2.*S_PI*ocESRCapOut()*ocCapOutValue(ivalue));
+}
+/**
+ * @brief
+ * @param
+ * @return
+ */
+inline double CapOut::ocOutRippleVolt(double curout, int16_t ncap)
+{
+    return (curout*ocESRCapOut())/ncap;
 }
--- FLySMPS/capout.h
+++ FLySMPS/capout.h
@@ -7,11 +7,19 @@
 class CapOut
 {
 public:
-    CapOut(double vr, double esr, double cpo, double vo, double crfq): volts_rippl(vr), esr_perc(esr), curr_peak_out(cpo), volts_out(vo), cros_frq_start_val(crfq){}
-    double ocESRCapOut();
-    double ocCapOutValue(const InputValue& ivalue);
-    double ocCurrOurRMS(const FBPT &fbptval);
-    double ocCapOutLoss(const FBPT &fbptval);
+    CapOut(double vr, double esr,
+           double cpo, double vo,
+           double crfq):
+        volts_rippl(vr), esr_perc(esr),
+        curr_peak_out(cpo), volts_out(vo),
+        cros_frq_start_val(crfq)
+    {}
+    inline double ocESRCapOut();
+    inline double ocCapOutValue(const InputValue& ivalue);
+    inline double ocCurrOurRMS(const FBPT &fbptval);
+    inline double ocZeroFreqCapOut(const InputValue &ivalue);
+    inline double ocOutRippleVolt(double curout, int16_t ncap);
+    inline double ocCapOutLoss(const FBPT &fbptval);
 
 private:
     double volts_rippl;
@@ -19,8 +27,8 @@ private:
     double curr_peak_out;
     double volts_out;
     double cros_frq_start_val;//good starting value for crossover frequency(1/20 to 1/10)
-    double ocTimeCapCharg(const InputValue& ivalue);
-    double ocCurrCap();
+    inline double ocTimeCapCharg(const InputValue& ivalue);
+    inline double ocCurrCap();
 };
 
 #endif // CAPOUT_H
