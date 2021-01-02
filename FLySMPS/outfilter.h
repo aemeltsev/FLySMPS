e3d0aea6ab88f06c7c6f93fd3ae58098230316fe
Tony Emeltsev <aemeltsev@gmail.com>
2021-01-02 16:17:32 +0300

    rewrited calculate filter logic

--- FLySMPS/outfilter.h
+++ FLySMPS/outfilter.h
@@ -11,99 +11,107 @@
 class OutFilter
 {
 public:
-    /**
-     * @brief OutFilter -
-     * @param fr -
-     * @param cap -
-     */
-    OutFilter(int16_t fr, double cap):
-        freq(fr), capacity(cap)
-    {}
-    /**
-     * @brief ofAngularCutFreq - Filter angular cut frequency
-     * @return
-     */
-    inline double ofAngularCutFreq() const {return 2*M_PI*freq;}
-    /**
-     * @brief ofLoadResistance - Resistance in Ohms
-     * @return
-     */
-    inline double ofLoadResistance() const {return 1./(M_SQRT2 * capacity * ofAngularCutFreq());}
-    /**
-     * @brief ofInductance - Inductance in H
-     * @return
-     */
-    inline double ofInductance() const {return  (ofLoadResistance() * M_SQRT2)/(ofAngularCutFreq());}
-    /******************************************************************/
-    /**
-     * @brief ofQualityFactor - Q factor
-     * @return
-     */
-    inline double ofQualityFactor(){return ofLoadResistance() * sqrt(capacity/ofInductance());}
-    /**
-     * @brief ofDampingRatio - The damping ratio(\zeta)
-     * @return
-     */
-    inline double ofDampingRatio(){return ofInductance()/(2 * ofQualityFactor());}
-    /**
-     * @brief ofCutOffFreq - Cutoff Frequency
-     * @return
-     */
-    inline double ofCutOffFreq(){return 1./(2*M_PI*qSqrt(capacity * ofInductance()));}
-    /**
-     * @brief ofOutRipplVolt - Output ripple voltage
-     * @return
-     */
-    inline double ofOutRipplVolt()
-    {
-        return (1./(ofAngularCutFreq()*capacity))/((1./(ofAngularCutFreq()*capacity))+(ofAngularCutFreq()*ofInductance()));
-    }
-    /**
-     * @brief ofPlotArray - Filling of data table
-     * @param tabmap - ptr to map object
-     * @param begin  - begin frequency point
-     * @param end - end frequency point
-     * @param step - frequency step
-     */
-    void ofPlotArray(QVector<double> &magnitudevector, QVector<double> &phasevector, int16_t begin, int16_t end, int16_t step)
-    {
-        for(int16_t ind=begin; ind<end; ind+=step)
-        {
-            magnitudevector.push_back(ofTransFuncGainDB(ind));
-            phasevector.push_back(ofTransfFuncPhase(ind));
-        }
-    }
-private:
-    /**
-     * @brief ofTransferFunc
-     * @param frq
-     * @return
-     */
-    inline double ofTransferFunc(int16_t frq)
-    {
-        return 1./qSqrt(1 + qPow(((2*M_PI*frq)/ofAngularCutFreq()), 4));
-    }
-    /**
-     * @brief ofFilterGainDB - Gain in decibels
-     * @param frq - frequency
-     * @return
-     */
-    inline double ofTransFuncGainDB(int16_t frq)
-    {
-        return 20*std::log10(ofTransferFunc(frq));
-    }
-    /**
-     * @brief ofTransfFuncPhase
-     * @param frq
-     * @return
-     */
-    inline double ofTransfFuncPhase(int16_t frq)
-    {
-        return M_J*(qAtan((2*M_PI*frq)/ofAngularCutFreq())*(M_PI_DEG/M_PI));
-    }
+        /**
+         * @brief OutFilter -
+         * @param fr -
+         * @param cap -
+         */
+        OutFilter(int16_t fr, int16_t rload):
+            m_freq(fr), m_rload(rload)
+        {}
 
-    int16_t freq=0;
-    double capacity=0;
+        /**
+         * @brief ofAngularCutFreq - Filter angular cut frequency
+         * @return
+         */
+        inline double ofAngularCutFreq() const {return 2*M_PI*m_freq;}
+        /**
+         * @brief ofCapacitor - Capacitance in F
+         * @return
+         */
+        inline double ofCapacitor() const {return 1./(M_SQRT2 * m_rload * ofAngularCutFreq());}
+        /**
+         * @brief ofInductor - Inductance in H
+         * @return
+         */
+        inline double ofInductor() const {return  (m_rload * M_SQRT2)/(ofAngularCutFreq());}
+        /******************************************************************/
+        /**
+         * @brief ofQualityFactor - Q factor
+         * @return
+         */
+        inline double ofQualityFactor(){return m_rload * qSqrt(ofCapacitor()/ofInductor());}
+        /**
+         * @brief ofDampingRatio - The damping ratio(\zeta)
+         * @return
+         */
+        inline double ofDampingRatio(){return 1./(2 * ofQualityFactor());}
+        /**
+         * @brief ofCutOffFreq - Cutoff Frequency
+         * @return
+         */
+        inline double ofCutOffFreq(){return 1./(2*M_PI*qSqrt(ofCapacitor() * ofInductor()));}
+        /**
+         * @brief ofOutRipplVolt - Output ripple voltage
+         * @return
+         */
+        inline double ofOutRipplVolt()
+        {
+            return (1./(ofAngularCutFreq()*ofCapacitor()))/((1./(ofAngularCutFreq()*ofCapacitor()))+(ofAngularCutFreq()*ofInductor()));
+        }
+
+        /**
+         * @brief ofPlotArray - Filling of data table
+         * @param tabmap - ptr to map object
+         * @param begin  - begin frequency point
+         * @param end - end frequency point
+         * @param step - frequency step
+         */
+        void ofPlotArray(QVector<double> &magnitudevector, QVector<double> &phasevector, int16_t begin, int16_t end, int16_t step)
+        {
+            for(int16_t ind=begin; ind<end; ind+=step)
+            {
+                magnitudevector.push_back(ofTFMagnitudeGain(ind));
+                phasevector.push_back(ofTFphase(ind));
+            }
+        }
+private:
+        inline double ofTFMagnitude(int16_t freq)
+        {
+            double omega = (2*M_PI*freq)/ofAngularCutFreq();
+            double first_denom = 1-qPow(omega, 2);
+            double second_denom = (1/ofQualityFactor())*omega;
+            return 1./(first_denom+second_denom);
+        }
+
+        inline double ofTFMagnitudeGain(int16_t freq)
+        {
+            return 20*std::log10(ofTFMagnitude(freq));
+        }
+
+        inline double ofTFphase(int16_t freq)
+        {
+            double omega = (2*M_PI*freq)/ofAngularCutFreq();
+            double numerator = (1/ofQualityFactor())*omega;
+            double denominator = 1-qPow(omega, 2);
+            return (-1)*qAtan(numerator/denominator);
+        }
+        /*        inline double ofTransferFunc(int16_t m_freq)
+                {
+                    return 1./qSqrt(1 + qPow(((2*M_PI*m_freq)/ofAngularCutFreq()), 4));
+                }
+                inline double ofTransFuncGainDB(int16_t frq)
+                {
+                    return 20*std::log10(ofTransferFunc(frq));
+                }
+                inline double ofTransfFuncPhase(int16_t frq)
+                {
+                    return M_J*(qAtan((2*M_PI*frq)/ofAngularCutFreq())*(M_PI_DEG/M_PI));
+                }
+        */
+
+        int16_t m_freq=0;
+        int16_t m_rload=0;
 };
 
 #endif // OUTFILTER_H
