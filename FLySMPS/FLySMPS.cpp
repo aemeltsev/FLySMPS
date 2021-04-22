296c77a4eb38988829a7d050959cbac52e5a9058
Tony Emeltsev <aemeltsev@gmail.com>
2021-04-22 13:59:21 +0300

    feature: add input variables converted

--- FLySMPS/FLySMPS.cpp
+++ FLySMPS/FLySMPS.cpp
@@ -18,3 +18,40 @@ void FLySMPS::initInputValues()
 {
 
 }
+
+double FLySMPS::convertToValues(const QString &input)
+{
+    QRegExp reg = QRegExp("^(.+)([pnμumKM])$");
+    reg.indexIn(input);
+
+    if(reg.captureCount() == 2)
+    {
+        QString strValue = reg.cap(1);
+        QString unit = reg.cap(2);
+        bool ok = false;
+        double value = strValue.toDouble(&ok);
+
+        if(!ok){return input.toDouble();}
+
+        if(unit == "p"){
+            return (value*1e-12);
+        }
+        else if(unit == "n"){
+            return (value*1e-9);
+        }
+        else if(unit == "μ" || unit == "u"){
+            return (value*1e-6);
+        }
+        else if(unit == "m"){
+            return (value*1e-3);
+        }
+        else if(unit == "K"){
+            return (value*1e3);
+        }
+        else if(unit == "M"){
+            return (value*1e6);
+        }
+        return value;
+    }
+    return 0;
+}
