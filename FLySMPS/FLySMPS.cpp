86e741e0d184618f31f4edfbc5dfeb88416a667f
Tony Emeltsev <aemeltsev@gmail.com>
2021-04-22 13:10:30 +0300

    chore: initialize constructor

--- FLySMPS/FLySMPS.cpp
+++ FLySMPS/FLySMPS.cpp
@@ -3,13 +3,18 @@
 
 FLySMPS::FLySMPS(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::FLySMPS)
+    ui(new Ui::FLySMPS),
+    m_psolve(new PowSuppSolve)
 {
     ui->setupUi(this);
-
+    initInputValues();
 }
 
 FLySMPS::~FLySMPS()
 {
 }
 
+void FLySMPS::initInputValues()
+{
+
+}
