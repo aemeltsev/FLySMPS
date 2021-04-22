79008ea0efd0dc5c1ae57c267cb1c5e221f9b0e3
Tony Emeltsev <aemeltsev@gmail.com>
2021-04-22 20:48:06 +0300

    feature: input initialise

--- FLySMPS/FLySMPS.cpp
+++ FLySMPS/FLySMPS.cpp
@@ -16,7 +16,44 @@ FLySMPS::~FLySMPS()
 
 void FLySMPS::initInputValues()
 {
+    m_psolve->m_indata.input_volt_ac_max = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->VACmax->text())));
+    m_psolve->m_indata.input_volt_ac_min = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->VACmin->text())));
+    m_psolve->m_indata.freq_line = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->FLine->text())));
+    m_psolve->m_indata.freq_switch = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->FSw->text())));
+    m_psolve->m_indata.temp_amb = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->Tamb->text())));
+    m_psolve->m_indata.volt_out_one = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->VOut1->text())));
+    m_psolve->m_indata.curr_out_one = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->IOut1->text())));
+    m_psolve->m_indata.volt_out_two = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->VOut2->text())));
+    m_psolve->m_indata.curr_out_two = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->IOut2->text())));
+    m_psolve->m_indata.volt_out_three = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->VOut3->text())));
+    m_psolve->m_indata.curr_out_three = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->IOut3->text())));
+    m_psolve->m_indata.volt_out_four = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->VOut4->text())));
+    m_psolve->m_indata.curr_out_four = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->IOut4->text())));
+    m_psolve->m_indata.volt_out_aux = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->VAux->text())));
+    m_psolve->m_indata.curr_out_aux = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->IAux->text())));
+    m_psolve->m_indata.eff = convertToValues(static_cast<QString>(ui->Eff->text()));
+    m_psolve->m_indata.mrgn = convertToValues(static_cast<QString>(ui->Eff->text())); /* TMP after use real value */
 
+    auto outPwr =
+        [=](double mrg)
+    {
+            return ((m_psolve->m_indata.volt_out_one * m_psolve->m_indata.curr_out_one)
+                    +(m_psolve->m_indata.volt_out_two * m_psolve->m_indata.curr_out_two)
+                    +(m_psolve->m_indata.volt_out_three * m_psolve->m_indata.curr_out_three)
+                    +(m_psolve->m_indata.volt_out_four * m_psolve->m_indata.curr_out_four)
+                    *(m_psolve->m_indata.volt_out_aux * m_psolve->m_indata.curr_out_aux)) * mrg;
+    };
+    m_psolve->m_indata.power_out_max = outPwr(m_psolve->m_indata.mrgn);
+
+    m_psolve->m_indata.refl_volt_max = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->ReflVoltage->text())));
+    m_psolve->m_indata.voltage_spike = static_cast<int16_t>(convertToValues(static_cast<QString>(ui->VSpike->text())));
+    m_psolve->m_indata.eff_transf = convertToValues(static_cast<QString>(ui->EffTransf->text()));
+    m_psolve->m_indata.volt_diode_drop_sec = convertToValues(static_cast<QString>(ui->VoltDropSec->text()));
+    m_psolve->m_indata.volt_diode_drop_bridge = convertToValues(static_cast<QString>(ui->VoltBridgeDrop->text()));
+    m_psolve->m_indata.leakage_induct = convertToValues(static_cast<QString>(ui->LeakageInduct->text()));
+    /*m_psolve->m_indata.sec_voltage_ripple = convertToValues(static_cast<QString>(ui->));
+    m_psolve->m_indata.sec_esr_perc = convertToValues(static_cast<QString>(ui->));
+    m_psolve->m_indata.sec_crfq_value = convertToValues(static_cast<QString>(ui->));*/
 }
 
 double FLySMPS::convertToValues(const QString &input)
