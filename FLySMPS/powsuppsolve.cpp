858885c95c8db2d75e4b820772ae8dcdf74dd721
Tony Emeltsev <aemeltsev@gmail.com>
2021-04-23 18:15:02 +0300

    chore: review initialize data object

--- FLySMPS/powsuppsolve.cpp
+++ FLySMPS/powsuppsolve.cpp
@@ -1,32 +1,22 @@
 #include "powsuppsolve.h"
 
 PowSuppSolve::PowSuppSolve(QObject *parent)
+    :QObject(parent)
 {
     m_db.reset(new DBridge);
     m_bc.reset(new BCap);
     m_pm.reset(new PMosfet);
     m_ptpe.reset(new PulseTransPrimaryElectr());
     m_ptsw.reset(new PulseTransSecondWired());
-
-    m_od = new ODiode();
-    m_fod = new FullOutDiode();
-    m_oc = new OCap();
-    m_foc = new FullOutCap();
-    m_of = new FullOutFilter();
-    m_pssm = new PowerStageSmallSignalModel();
-    m_ofs = new OptocouplerFedbackStage();
-
-
+    //m_od = new ODiode();
+    m_fod.reset(new FullOutDiode);
+    //m_oc = new OCap();
+    m_foc.reset(new FullOutCap);
+    m_of.reset(new FullOutFilter);
+    m_pssm.reset(new PowerStageSmallSignalModel);
+    m_ofs.reset(new OptocouplerFedbackStage);
 }
 
 PowSuppSolve::~PowSuppSolve()
-{
-    delete m_od;
-    delete m_fod;
-    delete m_oc;
-    delete m_foc;
-    delete m_of;
-    delete m_pssm;
-    delete m_ofs;
-}
+{}
 
