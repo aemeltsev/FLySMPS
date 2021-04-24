97aa1fd7d82deb0b30ac9b6d85886974ea77ed9b
Tony Emeltsev <aemeltsev@gmail.com>
2021-04-24 13:08:23 +0300

    feat: calculate input circuits

--- FLySMPS/powsuppsolve.cpp
+++ FLySMPS/powsuppsolve.cpp
@@ -20,3 +20,50 @@ PowSuppSolve::PowSuppSolve(QObject *parent)
 PowSuppSolve::~PowSuppSolve()
 {}
 
+void PowSuppSolve::calcInputNetwork()
+{
+    m_isSolveRunning = true;
+    emit startCalcInputNetwork();
+
+    if(!m_isSolveRunning){
+        emit calcCanceled();
+        return;
+    }
+
+    QScopedPointer<BulkCap> b_cap(new BulkCap(m_indata.input_volt_ac_max,
+                                              m_indata.input_volt_ac_min,
+                                              m_indata.eff,
+                                              static_cast<float>(m_indata.power_out_max),
+                                              m_indata.freq_line));
+    m_bc->delta_t = b_cap->DeltaT();
+    m_bc->charg_time = b_cap->ChargTime();
+    m_bc->bcapacitor_value = b_cap->CapValue();
+    m_bc->load_curr_max = b_cap->ILoadMax();
+    m_bc->load_curr_min = b_cap->ILoadMin();
+    m_bc->bcapacitor_peak_curr = b_cap->IBulkCapPeak();
+    m_bc->input_min_voltage = b_cap->VMinInp();
+    m_bc->input_dc_min_voltage = b_cap->VDCMin();
+
+    QScopedPointer<DiodeBridge> b_diode(new DiodeBridge(m_indata.input_volt_ac_max,
+                                                        m_indata.input_volt_ac_min,
+                                                        m_indata.eff,
+                                                        static_cast<float>(m_indata.power_out_max),
+                                                        m_indata.freq_line));
+    /**< 1. Set capacitor params */
+    b_diode->setBcapParam(m_bc->bcapacitor_peak_curr, m_bc->charg_time);
+    /**< 2. Fill the structure */
+    m_db->diode_peak_curr = b_diode->IDiodePeak();
+    m_db->diode_curr_slope = b_diode->DiodeCurrentSlope();
+    m_db->diode_cond_time = b_diode->DiodeConductTime();
+    m_db->load_avg_curr = b_diode->ILoadAVG();
+    m_db->diode_avg_curr = b_diode->IDiodeAVG();
+    m_db->diode_rms_curr = b_diode->IDiodeRMS();
+    m_db->diode_rms_curr_tot = b_diode->IDiodeRMSTot();
+    m_db->in_min_rms_voltage = b_diode->MinPeakInVoltage();
+    m_db->in_max_rms_voltage = b_diode->MaxPeakInVoltage();
+    /**< 3. Set capacitor RMS current */
+    m_bc->bcapacitor_rms_curr = b_cap->IBulkCapRMS(m_db->diode_avg_curr, m_db->diode_cond_time);
+
+    emit finishedInputNetwork();
+    m_isSolveRunning = false;
+}
