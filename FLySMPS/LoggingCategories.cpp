eb52fd545114eb8851e6f74d2eaa9f0296eee1f6
Tony Emeltsev <aemeltsev@gmail.com>
2021-05-17 19:08:01 +0300

    chore: remove old loging implementation files

--- FLySMPS/LoggingCategories.cpp
+++
@@ -1,6 +0,0 @@
-#include "LoggingCategories.h"
-
-Q_LOGGING_CATEGORY(logDebug, "Debug")
-Q_LOGGING_CATEGORY(logInfo, "Info")
-Q_LOGGING_CATEGORY(logWarning, "Warning")
-Q_LOGGING_CATEGORY(logCritical, "Critical")
