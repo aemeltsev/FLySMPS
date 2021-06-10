2663b0a79665d9fe49bcd6065cf3d59ac6978421
Tony Emeltsev <aemeltsev@gmail.com>
2021-06-10 15:46:58 +0300

    chore: fix qcustomplot file contain

--- FLySMPS/qcustomplot.cpp
+++ FLySMPS/qcustomplot.cpp
@@ -23,7 +23,7 @@
 **          Version: 2.0.1                                                **
 ****************************************************************************/
 
-#include "inc/qcustomplot.h"
+#include "qcustomplot.h"
 
 
 /* including file 'src/vector2d.cpp', size 7340                              */
