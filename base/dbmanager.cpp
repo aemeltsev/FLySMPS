dcde78c382afc7d780e582cb057906786643713b
Tony Emeltsev <aemeltsev@gmail.com>
2025-02-17 14:17:46 +0300

    Add class DBManager in db namespace with common properties for communication with datebase.

--- base/dbmanager.cpp
+++ base/dbmanager.cpp
@@ -1,22 +1,120 @@
-#include <base/dbmanager.h>
+﻿#include <base/dbmanager.h>
+#include <inc/loggercategories.h>
 
-DBManager::DBManager(QObject *parent)
+QString db::DBManager::DBASE_COMMON_NAME = QString("base.db");
+
+db::DBManager::DBManager(QObject *parent)
+    :QObject(parent)
 {
-
+    allocateDb();
 }
 
-bool DBManager::isOpen() const
+db::DBManager::~DBManager()
+{
+    closeAll();
+}
+
+bool db::DBManager::isOpen() const
 {
     return m_db.isOpen();
 }
 
-bool DBManager::tableExists(const QString &table)
+QString db::DBManager::sql(const char *sql) const
 {
-    QSqlQuery q(QString::fromLatin1("SELECT name FROM sqlite_master WHERE type='table' AND name='%1'").arg(table), m_db);
+    return QString::fromLatin1(sql);
+}
+
+bool db::DBManager::tableExists(const QString &table)
+{
+    QSqlQuery q(QString::fromLatin1("SELECT name FROM sqlite_master WHERE type = \"table\" AND name = \"%1\"").arg(table), m_db);
     if(!q.exec())
     {
-        qDebug() << QString::fromLatin1("'table' exist failed:").arg(table) << q.lastError();
+        qInfo(logCritical()) << QString::fromLatin1("'table' exist failed: \"%1\"").arg(table) << q.lastError();
         return false;
     }
     return q.next();
+}
+
+QSqlDatabase &db::DBManager::db()
+{
+    return m_db;
+}
+
+void db::DBManager::closeAll()
+{
+    QString connectionName = m_db.connectionName();
+    m_db.close();
+    QSqlDatabase::removeDatabase(connectionName);
+}
+
+void db::DBManager::setLastError(const QString &msg)
+{
+    if(!msg.isEmpty()){
+        qCritical(logCritical()) << QString::fromLatin1("DB error: \"%1\"").arg(msg);
+    }
+}
+
+bool db::DBManager::beginTransaction()
+{
+    if(!m_db.isOpen()){
+        setLastError(QString("Database is not open"));
+        return false;
+    }
+    if(!m_db.transaction()){
+        setLastError(m_db.lastError().text());
+        return false;
+    }
+    return true;
+}
+
+bool db::DBManager::rollback()
+{
+    if(!m_db.isOpen()){
+        setLastError(QString("Database is not open"));
+        return false;
+    }
+    if(!m_db.rollback()){
+        setLastError(m_db.lastError().text());
+        return false;
+    }
+    return true;
+}
+
+bool db::DBManager::endTransaction()
+{
+    if(!m_db.isOpen()){
+        setLastError(QString("Database is not open"));
+        return false;
+    }
+    if(!m_db.commit()){
+        setLastError(m_db.lastError().text());
+        return false;
+    }
+    return true;
+}
+
+bool db::DBManager::dropTable(const QString &table)
+{
+    QString sqlQuery = sql("DROP TABLE %1").arg(table);
+    QSqlQuery q(sqlQuery, m_db);
+    if(!q.exec()){
+        setLastError(q.lastError().text());
+        return false;
+    }
+    return true;
+}
+
+void db::DBManager::allocateDb()
+{
+    m_db = QSqlDatabase::addDatabase("QSQLITE", connectionName());
+    m_db.setDatabaseName(dbName());
+
+    if(!m_db.open()){
+        setLastError(m_db.lastError().text());
+    }
+
+    QSqlQuery q("PRAGMA foreign_keys = ON", m_db);
+    if(!q.exec()){
+        setLastError((q.lastError().text()));
+    }
 }
--- base/dbmanager.h
+++ base/dbmanager.h
@@ -6,42 +6,47 @@
 #include <QSqlQuery>
 #include <QSqlError>
 #include <QSqlRecord>
+#include <QMutex>
 
 #include <QtDebug>
 
-#include "defines.h"
+namespace db {
 
 class DBManager: public QObject
 {
     Q_OBJECT
-    QSqlDatabase m_db;
 
 public:
-
-    explicit DBManager(QObject *parent = nullptr);
-    bool initSQL(QString dbPath="");
-    bool createTable(const QString& table, const QStringList& columns);
     bool isOpen() const;
     const QString& lastError() const;
-    bool addCore(const Core& core_data);
-    bool addMaterial(const Material& material_data);
-    bool addGapping(const Gapping& gap_data, bool gapped=true);
-    bool addToroidGeometry(const GeometryToroidal& geometry);
-    bool addUUGeometry(const GeometryUU& geometry);
-    bool addEEGeometry(const GeometryEE& geometry);
-    bool addETDGeometry(const GeometryETD& geometry);
-    bool removeCore(const QString& name);
-    bool removeMaterial(const QString& name);
-    bool removeGeometry(const QString& name, CoreType crtype);
-    bool getCoreProperties(const QString& core_name,
-                           const QString& material_name,
-                           Core& out_core,
-                           Material& out_material,
-                           Gapping& out_gapp,
-                           bool gapped=true);
-    bool getCoreGeometry(const QString& core_name, QVector<uint16_t> out_gm);
-protected:
-    bool tableExists(const QString& table);
-};
+    virtual QString dbName() const = 0;
+    QString sql(const char* sql) const;
 
+protected:
+    explicit DBManager(QObject *parent = nullptr);
+    virtual ~DBManager();
+
+    bool tableExists(const QString& table);
+    virtual QString connectionName() const = 0;
+    QSqlDatabase& db();
+    void closeAll();
+    void setLastError(const QString& msg);
+
+    bool createTable(const QString& table, const QStringList& columns);
+    bool beginTransaction();
+    bool rollback();
+    bool endTransaction();
+
+    bool dropTable(const QString& table);
+
+    QMutex dbMutex_;
+    static QString DBASE_COMMON_NAME;
+    virtual int lastInsertId(const QString& table) = 0;
+
+private:
+    void allocateDb();
+    QSqlDatabase m_db;
+
+};
+} //namespace db
 #endif //DBMANAGER_H
