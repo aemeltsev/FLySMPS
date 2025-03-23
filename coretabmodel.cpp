6473951cba91185c5c904c37d578830c60c31360
Tony Emeltsev <aemeltsev@gmail.com>
2025-03-23 22:13:28 +0300

    Add prototype and implement add one row and series rows.

--- coretabmodel.cpp
+++ coretabmodel.cpp
@@ -1,6 +1,81 @@
 #include "coretabmodel.h"
 
-CoreTabModel::CoreTabModel()
+CoreTabModel::CoreTabModel(QObject *parent)
 {
 
 }
+
+int CoreTabModel::rowCount(const QModelIndex &parent) const
+{
+
+}
+
+int CoreTabModel::columnCount(const QModelIndex &parent) const
+{
+
+}
+
+QVariant CoreTabModel::data(const QModelIndex &index, int role) const
+{
+
+}
+
+bool CoreTabModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+
+}
+
+QVariant CoreTabModel::headerData(int section, Qt::Orientation orientation, int role) const
+{
+
+}
+
+Qt::ItemFlags CoreTabModel::flags(const QModelIndex &index) const
+{
+
+}
+
+void CoreTabModel::appendCoreRow(const int id, const QString &model, const QString &geom, const bool gapped, const QString &mat)
+{
+    CoreTableData core_data;
+    core_data[ID] = id;
+    core_data[MODEL] = model;
+    core_data[TYPE_GEOMETRY] = geom;
+    core_data[GAPPED] = gapped;
+    core_data[MATERIAL] = mat;
+    /* maybe add element to hash with properties core_data[SELECTION] = false */
+
+    int row = m_cores.count();
+
+    // Уведомляем представление о том, что будут добавлена новая строка
+    beginInsertRows(QModelIndex(), row, row);
+
+    m_cores.append(core_data);
+
+    // Завершаем добавление строки
+    endInsertRows();
+}
+
+void CoreTabModel::appendCoreRows(const QList<CoreTableItem> &items)
+{
+    int initial_row_count = m_cores.count();
+    int new_row_count = items.size();
+
+    // Уведомляем представление о том, что будут добавлена новая строка
+    beginInsertRows(QModelIndex(), initial_row_count, initial_row_count + new_row_count - 1);
+
+    for(const auto& item : items){
+        CoreTableData core_data;
+        core_data[ID] = item.m_id;
+        core_data[MODEL] = item.m_model;
+        core_data[TYPE_GEOMETRY] = item.m_geom;
+        core_data[GAPPED] = item.m_gapped;
+        core_data[MATERIAL] = item.m_mat;
+        /* maybe add element to hash with properties core_data[SELECTION] = false */
+        m_cores.append(core_data);
+    }
+
+    // Завершаем добавление строки
+    endInsertRows();
+}
+
--- coretabmodel.h
+++ coretabmodel.h
@@ -1,11 +1,69 @@
 #ifndef CORETABMODEL_H
 #define CORETABMODEL_H
 
+#include <QAbstractTableModel>
+#include "base/coremodel.h"
+
+struct CoreTableItem
+{
+    bool m_gapped;
+    int m_id;
+    QString m_model;
+    QString m_geom;
+    QString m_mat;
+
+    CoreTableItem(bool gapped, int id, QString model, QString geom, QString mat)
+        :m_gapped(gapped)
+        ,m_id(id)
+        ,m_model(model)
+        ,m_geom(geom)
+        ,m_mat(mat)
+    {}
+};
+
 
 class CoreTabModel : public QAbstractTableModel
 {
+    Q_OBJECT
+
 public:
-    CoreTabModel();
+    CoreTabModel(QObject* parent = nullptr);
+
+    int rowCount(const QModelIndex& parent) const;
+    int columnCount(const QModelIndex& parent) const;
+    QVariant data(const QModelIndex& index, int role) const;
+    bool setData(const QModelIndex& index, const QVariant& value, int role);
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+    Qt::ItemFlags flags(const QModelIndex& index) const;
+
+    void appendCoreRow(const int id, const QString& model, const QString& geom, const bool gapped, const QString& mat);
+    void appendCoreRows(const QList<CoreTableItem>& items);
+
+    db::CoreModel* getCoreModel();
+    db::Geometry* getGeometry();
+    db::Material* getMaterial();
+    db::Gapping* getGapping();
+
+private:
+    enum Column
+    {
+        ID = 0,
+        MODEL,
+        TYPE_GEOMETRY,
+        GAPPED,
+        MATERIAL
+    };
+
+    // list cores for view in table widget
+    typedef QHash<Column, QVariant> CoreTableData;
+    typedef QList<CoreTableData> Cores;
+    Cores m_cores;
+
+    // core model to send in main form
+    db::CoreModel* m_core_model;
+    db::Geometry* m_core_geometry;
+    db::Material* m_core_material;
+    db::Gapping* m_core_gapping;
 };
 
 #endif // CORETABMODEL_H
