543d8b2263c003b1667fa0bf74f7e447c523d209
Tony Emeltsev <aemeltsev@gmail.com>
2025-08-15 22:17:45 +0300

    Reimplement the main logger class. Use the singleton properties for construct logger object

--- src/logfilewriter.cpp
+++ src/logfilewriter.cpp
@@ -2,20 +2,49 @@
   See https://github.com/tvabramov/qt-sample-logging
 */
 #include <QTextStream>
+#include <QtDebug>
+#include <QThread>
 #include "inc/logfilewriter.h"
 
+// Static variable initialization
+LogFileWriter* LogFileWriter::m_instance = nullptr;
+
+// Implementation of the instance() method
+LogFileWriter* LogFileWriter::instance(QString prefix, qint64 maxsize) {
+    static QMutex mutex;
+    QMutexLocker locker(&mutex);
+    if (!m_instance) {
+        m_instance = new LogFileWriter(prefix, maxsize);
+    }
+    return m_instance;
+}
+
+// Implementation of the constructor
 LogFileWriter::LogFileWriter(QString prefix, qint64 maxsize, QObject *parent)
     :QObject(parent)
     ,m_prefix(prefix)
     ,m_max_file_size(maxsize)
     ,m_abort(false)
-{}
+{
+   /* // Initialize the log file
+        QString filename = QString("./%1_%2.log")
+                            .arg(m_prefix)
+                            .arg(QDateTime::currentDateTime().toString("yyyy-MM-dd"));
+        m_file.setFileName(filename);
+        m_file.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text);
+        if (m_file.pos() == 0) {
+            QTextStream out(&m_file);
+            out << "Time\tLevel\tSource\tMessage" << endl;
+            out.flush();
+        }*/
+}
 
 LogFileWriter::~LogFileWriter()
 {
     QMutexLocker locker(&m_mutex);
     m_abort = true;
     m_condition.wakeAll();
+    m_file.close();
 }
 
 void LogFileWriter::push(QDateTime timestamp, QtMsgType type, QString category, QString msg)
@@ -39,21 +68,18 @@ void LogFileWriter::main_loop()
 {
     forever
     {
-        m_mutex.lock();
-
+        QMutexLocker loker(&m_mutex);
         if(m_data.isEmpty() && !m_abort){
             m_condition.wait(&m_mutex);
         }
 
         bool abort = m_abort;
         QQueue<log_quantum> lqq = QQueue<log_quantum>(std::move(m_data));
-
         m_mutex.unlock();
 
         while(!lqq.isEmpty()){
 
             log_quantum lq = lqq.dequeue();
-
             //If file is not open, open him
             if(!m_file.isOpen()){
 
@@ -92,15 +118,12 @@ void LogFileWriter::main_loop()
             case QtCriticalMsg: out << "CRT "; break;
             case QtFatalMsg:    out << "FTL "; break;
             }
-            out << "\t";
-
-            out << lq.m_category << "\t";
-
             //Write into output category of the message and the message itself
-            out << lq.m_msg << endl;
+            out << "\t" << lq.m_category << "\t" << lq.m_msg << endl;
         }
 
         if(abort){
+            m_file.close();
             emit finished();
             return;
         }
